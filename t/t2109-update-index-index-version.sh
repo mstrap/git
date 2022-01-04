df4dcf3fd7ad22405061710162f57fb2bc66cba1
Marc Strapetz <marc.strapetz@syntevo.com>
2022-01-04 14:15:21 +0100

    t2109: basic update-index --index-version tests
    A set of basic tests related to 'git update-index --index-version' which
    capture under which conditions a format change is actually performed.
    
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>

---
+++ t/t2109-update-index-index-version.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+test_description='git update-index --index-version test.
+'
+
+. ./test-lib.sh
+
+sane_unset GIT_TEST_SPLIT_INDEX
+
+test_expect_success 'setup' '
+	echo content >file &&
+	git add file &&
+	git commit -m "initial import"
+'
+
+test_expect_success 'v2->v2 does not change index' '
+	git update-index --index-version 2 &&
+	test "$(test-tool index-version < .git/index)" = 2
+'
+
+test_expect_success 'v2->v3 does not change index' '
+	git update-index --index-version 2 &&
+	test "$(test-tool index-version < .git/index)" = 2
+'
+
+test_expect_success 'v3->v2 does not change index' '
+	echo content2 >file2 &&
+	git add -N file2 &&
+	test "$(test-tool index-version < .git/index)" = 3 &&
+	git update-index --index-version 2 &&
+	test "$(test-tool index-version < .git/index)" = 3
+'
+
+test_expect_success 'v3->v2 does change index' '
+	git rm --force file2 &&
+	git update-index --index-version 2 &&
+	test "$(test-tool index-version < .git/index)" = 2
+'
+
+test_expect_success 'v2->v4 does change index' '
+	git update-index --index-version 4 &&
+	test "$(test-tool index-version < .git/index)" = 4
+'
+
+test_expect_success 'v4->v2 does change index' '
+	git update-index --index-version 2 &&
+	test "$(test-tool index-version < .git/index)" = 2
+'
+
+test_done
